8b2b8ed062f41982aa6b3086a7d8eb0518ac9c6a
Linhehe <linhehe@Iin.local>
2015-06-23 20:40:05 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ var OnlineMessage = mongoose.model('OnlineMessage');
 
 Person.remove().exec();
 
-var person1 = new Person({email:"123456789@qq.com", password:"123"});
+var person1 = new Person({email:"123456789@qq.com", password:"123", sex: 'girl'});
 
 person1.save();
 
@@ -60,17 +60,44 @@ router.get('/sessions', function(req, res, next) {
 
 // 注册
 router.post('/sessions', function(req, res, next) {
-	// var person = new Person();
 
-	// person.email = req.body.email;
-	// person.password = req.body.password;
+	Person.findOne({email: req.body.email}, function(err,doc){
+		if(err != null){
+			next(err);
+		}
+		else{
+			if(doc != null){
+				console.log('find!!');
+				// 找到，不能注册
+				res.send('error');
+			}else{
+				console.log('no find!!');
+				// 找不到，可以注册
+				var user = new Person({email: req.body.email, password: req.body.password});
 
-	var person = new Person(req.body);
+				user.save(function(err, user){
+					if(err){ return next(err); }
 
-	person.save(function(err, person){
-		if (err) { return next(err); }
-		res.json(person);
+					res.json(user);
+					console.log(req.body);
+					console.log(req.body.email);
+				});
+			}
+		}
 	});
+});
+
+// 取数据
+router.get('/show', function(req, res, next){
+	Person.findOne({email: req.query.email}, function (err, doc) {
+		if(err != null){
+			next(err);
+		}
+		else{
+			res.json(doc);
+		}
+	});
+	console.log(req.query.email);
 });
 
 module.exports = router;
